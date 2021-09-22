a375be1e756f2c87489d76f3a887efc20e2fe267
wkropat <wkropat@gmail.com>
2021-09-22 10:54:30 -0700

    started js file

---
+++ script.js
@@ -0,0 +1,21 @@
+if user inputs r 
+if computer selects s
+    console.log (winner)
+    return counter+1 on winner attribute of playHistory function
+else 
+    console.log(loser)
+    return counter+1 on loser attribute of playHistory function
+else if user inputs p
+if computer selects s
+    console.log(loser)
+    return counter+1 on loser attribute of playHistory function
+else computer elects r
+    console.log(winner)
+    return counter+1 on winner attribute of playHistory function
+else 
+if computer selects p
+    console.log(winner)
+    return counter+1 on winner attribute of playHistory function
+else computer elects r
+    console.log(loser)
+    return counter+1 on loser attribute of playHistory function
